8ffdc2f882ac02326136e7f0e7608fc6371820b3
om <om@pop-os.localdomain>
2023-01-21 00:41:40 +0330

    Work on TCP and have problem on port recive packet

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
 cargo b --release
-sudo setcap net_cap_admin=eip ./target/release/rust-srt
+sudo setcap cap_net_admin=eip ./target/release/rust-srt
 ./target/release/rust-srt &
 pid=$!
 sudo ip addr add 192.168.0.1/24 dev tun0
 sudo ip link set up dev tun0
+trap "kill $pid" INT TERM
 wait $pid
 
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,42 @@
-extern crate tun_tap;
 use std::io;
 
 fn main() -> io::Result<()> {
-    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
-    let mut buf = vec![0u8; 1504]; // MTU + 4 for the header
-
+        let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+        let mut buf = vec![0u8; 1504]; // MTU + 4 for the header
+    loop {
     let nbytes = nic.recv(&mut buf[..])?;
-    println!("Read {} from {:x?}", nbytes , &buf[..nbytes]);
+        let eth_flag = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
+        if eth_proto != 0x0800 {
+            // not ipV4
+            continue;
+        }
 
+        
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) { 
+            Ok(p) => {
+            let src = p.source_addr();
+            let dst = p.destination_addr();
+            let proto = p.protocol();
+                if proto != 0x06 {
+                    // not TCP
+                    continue;
+                }
+ match etherparse::Ipv4HeaderSlice::from_slice(&buf[4+p.slice().len()..]) {
+            Ok(p) => {
+                println!("{} -> {} {}b of tcp to port {}",src, dst, p.slice().len(), p.destination_port());
+            }
+            Err(e) => {
+                eprintln!("ignoring weired tcp packet {:?}", e);
+            }
+        }
+            } 
+
+           
+            Err(e) => {
+                eprintln!("ignoring weired packet {:?}", e);
+            }
+        }
+    }
     Ok(())
 }
